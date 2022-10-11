402d18867a96618f1e66085d6dcd4d029dfe542d
austinpkugler <austinpkugler@gmail.com>
2022-10-11 00:04:39 -0700

    hw2: increase ast node functionality

--- hw2/Node.cpp
+++ hw2/Node.cpp
@@ -10,7 +10,50 @@ Node::Node(int tokenLineNumber)
     m_tokenLineNumber = tokenLineNumber;
 }
 
+Node::~Node()
+{
+    if (m_sibling != nullptr)
+    {
+        delete m_sibling;
+    }
+
+    for (auto &node : m_children)
+    {
+        delete node;
+    }
+}
+
+void Node::printTree()
+{
+
+}
+
+void Node::addChild(Node *node)
+{
+    m_children.push_back(node);
+}
+
 void Node::addSibling(Node *node)
 {
 
+}
+
+void Node::printNode(std::string tab)
+{
+    for (int i = 0; i < tab.length(); i++)
+    {
+        tab += ".   ";
+    }
+
+    std::cout << tab << "Child: " << std::endl;
+
+    for (auto child : m_children)
+    {
+        child->printNode(tab + ".   ");
+    }
+
+    if (m_sibling != nullptr)
+    {
+        m_sibling->printNode(tab);
+    }
 }
--- hw2/Node.hpp
+++ hw2/Node.hpp
@@ -6,19 +6,24 @@
 class Node
 {
     public:
-        typedef int OpKind;                                                                 // Kinds of Operators
-        enum NodeKind { DeclK, StmtK, ExpK };                                               // Kinds of Statements
-        enum DeclKind { VarK, FuncK, ParamK } ;                                             // Subkinds of Declarations
-        enum StmtKind { NullK, IfK, WhileK, ForK, CompoundK, ReturnK, BreakK, RangeK };     // Subkinds of Statements
-        enum ExpKind { OpK, ConstantK, IdK, AssignK, InitK, CallK };                        // Subkinds of Expressions
-        enum VarKind { None, Local, Global, Parameter, LocalStatic };                       // What kind of scoping is used
-        enum ExpType { Void, Integer, Boolean, Char, CharInt, Equal, UndefinedType };       // ExpType is used for type checking
+        typedef int OpKind;                                                                     // Kinds of Operators
+        enum class NodeKind { DeclK, StmtK, ExpK };                                             // Kinds of Statements
+        enum class DeclKind { VarK, FuncK, ParamK } ;                                           // Subkinds of Declarations
+        enum class StmtKind { NullK, IfK, WhileK, ForK, CompoundK, ReturnK, BreakK, RangeK };   // Subkinds of Statements
+        enum class ExpKind { OpK, ConstantK, IdK, AssignK, InitK, CallK };                      // Subkinds of Expressions
+        enum class VarKind { None, Local, Global, Parameter, LocalStatic };                     // What kind of scoping is used
+        enum class ExpType { Void, Integer, Boolean, Char, CharInt, Equal, UndefinedType };     // ExpType is used for type checking
 
         Node();
         Node(int tokenLineNumber);
+        ~Node();
+        void printTree();
+        void addChild(Node *node);
         void addSibling(Node *node);
 
     private:
+        void printNode(std::string tab);
+
         std::vector<Node *> m_children;
         Node *m_sibling;
         int m_tokenLineNumber;
