c81d6bf8961c8a79b0f14f3339a8bdc44ce68882
austinpkugler <austinpkugler@gmail.com>
2022-10-20 12:37:08 -0700

    hw3: remove some spaghet

--- hw3/src/Semantics/Is.cpp
+++ hw3/src/Semantics/Is.cpp
@@ -1,266 +1,118 @@
 #include "Is.hpp"
 
 bool isDecl(const Node *node)
-    {
-        if (node == nullptr || node->getNodeKind() != Node::Kind::Decl)
-        {
-            return false;
-        }
-        return true;
-    }
+{
+    return (node != nullptr && node->getNodeKind() == Node::Kind::Decl);
+}
 
 bool isFunc(const Node *node)
-    {
-        if (!isDecl(node))
-        {
-            return false;
-        }
-        Decl *decl = (Decl *)node;
-        if (decl->getDeclKind() != Decl::Kind::Func)
-        {
-            return false;
-        }
-        return true;
-    }
+{
+    if (!isDecl(node)) return false;
+    return (((Decl *)node)->getDeclKind() == Decl::Kind::Func);
+}
 
 bool isParm(const Node *node)
-    {
-        if (!isDecl(node))
-        {
-            return false;
-        }
-        Decl *decl = (Decl *)node;
-        if (decl->getDeclKind() != Decl::Kind::Parm)
-        {
-            return false;
-        }
-        return true;
-    }
+{
+    if (!isDecl(node)) return false;
+    return (((Decl *)node)->getDeclKind() == Decl::Kind::Parm);
+}
 
 bool isVar(const Node *node)
-    {
-        if (!isDecl(node))
-        {
-            return false;
-        }
-        Decl *decl = (Decl *)node;
-        if (decl->getDeclKind() != Decl::Kind::Var)
-        {
-            return false;
-        }
-        return true;
-    }
+{
+    if (!isDecl(node)) return false;
+    return (((Decl *)node)->getDeclKind() == Decl::Kind::Var);
+}
 
 bool isExp(const Node *node)
-    {
-        if (node == nullptr || node->getNodeKind() != Node::Kind::Exp)
-        {
-            return false;
-        }
-        return true;
-    }
+{
+    return (node != nullptr && node->getNodeKind() == Node::Kind::Exp);
+}
 
 bool isAsgn(const Node *node)
-    {
-        if (!isExp(node))
-        {
-            return false;
-        }
-        Exp *exp = (Exp *)node;
-        if (exp->getExpKind() != Exp::Kind::Asgn)
-        {
-            return false;
-        }
-        return true;
-    }
+{
+    if (!isExp(node)) return false;
+    return (((Exp *)node)->getExpKind() == Exp::Kind::Asgn);
+}
 
 bool isBinary(const Node *node)
-    {
-        if (!isExp(node))
-        {
-            return false;
-        }
-        Exp *exp = (Exp *)node;
-        if (exp->getExpKind() != Exp::Kind::Binary)
-        {
-            return false;
-        }
-        return true;
-    }
+{
+    if (!isExp(node)) return false;
+    return (((Exp *)node)->getExpKind() == Exp::Kind::Binary);
+}
 
 bool isCall(const Node *node)
-    {
-        if (!isExp(node))
-        {
-            return false;
-        }
-        Exp *exp = (Exp *)node;
-        if (exp->getExpKind() != Exp::Kind::Call)
-        {
-            return false;
-        }
-        return true;
-    }
+{
+    if (!isExp(node)) return false;
+    return (((Exp *)node)->getExpKind() == Exp::Kind::Call);
+}
 
 bool isConst(const Node *node)
-    {
-        if (!isExp(node))
-        {
-            return false;
-        }
-        Exp *exp = (Exp *)node;
-        if (exp->getExpKind() != Exp::Kind::Const)
-        {
-            return false;
-        }
-        return true;
-    }
+{
+    if (!isExp(node)) return false;
+    return (((Exp *)node)->getExpKind() == Exp::Kind::Const);
+}
 
 bool isId(const Node *node)
-    {
-        if (!isExp(node))
-        {
-            return false;
-        }
-        Exp *exp = (Exp *)node;
-        if (exp->getExpKind() != Exp::Kind::Id)
-        {
-            return false;
-        }
-        return true;
-    }
+{
+    if (!isExp(node)) return false;
+    return (((Exp *)node)->getExpKind() == Exp::Kind::Id);
+}
 
 bool isUnary(const Node *node)
-    {
-        if (!isExp(node))
-        {
-            return false;
-        }
-        Exp *exp = (Exp *)node;
-        if (exp->getExpKind() != Exp::Kind::Unary)
-        {
-            return false;
-        }
-        return true;
-    }
+{
+    if (!isExp(node)) return false;
+    return (((Exp *)node)->getExpKind() == Exp::Kind::Unary);
+}
 
 bool isUnaryAsgn(const Node *node)
-    {
-        if (!isExp(node))
-        {
-            return false;
-        }
-        Exp *exp = (Exp *)node;
-        if (exp->getExpKind() != Exp::Kind::UnaryAsgn)
-        {
-            return false;
-        }
-        return true;
-    }
+{
+    if (!isExp(node)) return false;
+    return (((Exp *)node)->getExpKind() == Exp::Kind::UnaryAsgn);
+}
 
 bool isStmt(const Node *node)
-    {
-        if (node == nullptr || node->getNodeKind() != Node::Kind::Stmt)
-        {
-            return false;
-        }
-        return true;
-    }
+{
+    return (node != nullptr && node->getNodeKind() == Node::Kind::Stmt);
+}
 
 bool isBreak(const Node *node)
-    {
-        if (!isStmt(node))
-        {
-            return false;
-        }
-        Stmt *stmt = (Stmt *)node;
-        if (stmt->getStmtKind() != Stmt::Kind::Break)
-        {
-            return false;
-        }
-        return true;
-    }
+{
+    if (!isStmt(node)) return false;
+    return (((Stmt *)node)->getStmtKind() == Stmt::Kind::Break);
+}
 
 bool isCompound(const Node *node)
-    {
-        if (!isStmt(node))
-        {
-            return false;
-        }
-        Stmt *stmt = (Stmt *)node;
-        if (stmt->getStmtKind() != Stmt::Kind::Compound)
-        {
-            return false;
-        }
-        return true;
-    }
+{
+    if (!isStmt(node)) return false;
+    return (((Stmt *)node)->getStmtKind() == Stmt::Kind::Compound);
+}
 
 bool isFor(const Node *node)
-    {
-        if (!isStmt(node))
-        {
-            return false;
-        }
-        Stmt *stmt = (Stmt *)node;
-        if (stmt->getStmtKind() != Stmt::Kind::For)
-        {
-            return false;
-        }
-        return true;
-    }
+{
+    if (!isStmt(node)) return false;
+    return (((Stmt *)node)->getStmtKind() == Stmt::Kind::For);
+}
 
 bool isIf(const Node *node)
-    {
-        if (!isStmt(node))
-        {
-            return false;
-        }
-        Stmt *stmt = (Stmt *)node;
-        if (stmt->getStmtKind() != Stmt::Kind::If)
-        {
-            return false;
-        }
-        return true;
-    }
+{
+    if (!isStmt(node)) return false;
+    return (((Stmt *)node)->getStmtKind() == Stmt::Kind::If);
+}
 
 bool isRange(const Node *node)
-    {
-        if (!isStmt(node))
-        {
-            return false;
-        }
-        Stmt *stmt = (Stmt *)node;
-        if (stmt->getStmtKind() != Stmt::Kind::Range)
-        {
-            return false;
-        }
-        return true;
-    }
+{
+    if (!isStmt(node)) return false;
+    return (((Stmt *)node)->getStmtKind() == Stmt::Kind::Range);
+}
 
 bool isReturn(const Node *node)
-    {
-        if (!isStmt(node))
-        {
-            return false;
-        }
-        Stmt *stmt = (Stmt *)node;
-        if (stmt->getStmtKind() != Stmt::Kind::Return)
-        {
-            return false;
-        }
-        return true;
-    }
+{
+    if (!isStmt(node)) return false;
+    return (((Stmt *)node)->getStmtKind() == Stmt::Kind::Return);
+}
 
 bool isWhile(const Node *node)
-    {
-        if (!isStmt(node))
-        {
-            return false;
-        }
-        Stmt *stmt = (Stmt *)node;
-        if (stmt->getStmtKind() != Stmt::Kind::While)
-        {
-            return false;
-        }
-        return true;
-    }
+{
+    if (!isStmt(node)) return false;
+    return (((Stmt *)node)->getStmtKind() == Stmt::Kind::While);
+}
